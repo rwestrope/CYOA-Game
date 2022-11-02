21dd3607c01437d469e9a0ae068d8468b6e0d408
rwestrope <rewestrope@gmail.com>
2022-11-02 11:32:03 -0500

    Started Combat File

--- character_classes.py
+++ character_classes.py
@@ -1,12 +1,12 @@
 import random
 class Character:
 
-    def __init__(self, type, name, training, specialty):
+    def __init__(self, type, name):
         self.data = []
         self.type = type 
         self.name = name
-        self.training = training
-        self.specialty = specialty
+        self.training = ""
+        self.specialty = ""
 
 
     Character_Types = ["Farmer","Hunter","Warrior"]
@@ -92,11 +92,11 @@ class Character:
                 player.Warrior_Trainings.remove('Swordsman')
                 self.FailedTraining()
     
-
+    
 if __name__ == "__main__":
     x=""
 
-    player = Character("","","","")
+    player = Character("","")
 
     print("What type of character would you like to play as?")
     print("Character Types:", Character.getcharactertypes())
