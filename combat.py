907484a373a48899a8c86644719033019e363179
Affergot <ralphsartin721@gmail.com>
2022-11-03 16:49:44 -0500

    Combat attempt lol

--- combat.py
+++ combat.py
@@ -1,15 +1,27 @@
-
+import random
 
 class Enemy:
 
-    def __init__(self):
+    def __init__(self, name, health, damage):
         self.data =""
+        self.name = name
+        self.health = health
+        self.damage = damage
 
-    
+    #def InitiateBattle(self):
+
+
+    def damagedealt(self):
+        damagedealt = random.randint(self.damage - 1, self.damage + 2)
+        return damagedealt
 
 if __name__ == "__main__":
 
-    Goomba = Enemy()
+    Goomba = Enemy("Goomba", 10, 3)
 
     print("You walked into a battle")
-    
+    player.InitiateBattle()
+    print("Goomba goes first")
+    while current_opponent.health > 0 and player.health > 0:
+        player.health = player.health - Goomba.damagedealt()
+        print(f"{Goomba.name} dealt {damagedealt}")
